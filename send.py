cfbc5175ab0f5db696379a6622dc12a280caf981
Abus <>
2022-01-16 16:41:58 +0200

    Added multi threading

--- send.py
+++ send.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import queue
 import sys
 from pathlib import Path
 import argparse
 from sms77api.Sms77api import Sms77api
 import os
 from dotenv import load_dotenv
+from queue import Queue
+from threading import Thread
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+class SmsWorker(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+    
+    def run(self):
+        while True:
+            number, message = self.queue.get()
+            try:
+                send_message(number, message)
+            finally:
+                self.queue.task_done()
 
 load_dotenv()
 SMS77IO_API_KEY = os.getenv('SMS77IO_API_KEY')
@@ -34,32 +54,45 @@ def count_numbers(numbers):
 
 def send_message(numbers, message):
 
-    numbers = load_file(numbers)
+    # numbers = load_file(numbers)
     message = load_file(message)
 
-    for number in numbers:
-        res = client.sms(number, message[0], {'json': True})
-        if(res['success'] == 100):
-            print(f'\033[92m Message Sent succesfully to {number} \033[00m')
-            log("success", "{}\n".format(number))
-        else:
-            print("\033[91m Message not sent to {} - {} \033[00m".format(number, res['messages'][0]['error_text']))
-            log("error", "{}\n".format(number))
+    # for number in numbers:
+    res = client.sms(number, message[0], {'json': True})
+    if(res['success'] == 100):
+        print(f'\033[92m Message Sent succesfully to {number} \033[00m')
+        log("success", "{}\n".format(number))
+    else:
+        print("\033[91m Message not sent to {} - {} \033[00m".format(number, res['messages'][0]['error_text']))
+        log("error", "{}\n".format(number))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send SMS with sms77.io')
     parser.add_argument("-n", "--numbers", help="file that contains phone numbers")
     parser.add_argument("-m", "--message", help="file that contains the message")
     parser.add_argument("-b", "--balance", action="store_true", help="See the available balance")
-    parser.add_argument("-t", "--total", help="Count numbers avalable to send")
+    parser.add_argument("-c", "--count", help="Count numbers available to send")
+    parser.add_argument("-t", "--threads", default=5, type=int, help="Threads to boost the speed")
     args = parser.parse_args()
 
     if args.numbers and args.message:
-        send_message(args.numbers, args.message)
+        queue = Queue()
+        for x in range(args.threads):
+            worker = SmsWorker(queue)
+            worker.daemon = True
+            worker.start()
+        
+        numbers = load_file(args.numbers)
+
+        for number in numbers:
+            logger.info('Queueing {} {}'.format(number, args.message))
+            queue.put((number, args.message))
+        queue.join()
+
     elif args.balance:
         print("Your available balance is {} euro\n".format(show_balance()))
-    elif args.total:
-        print("{} Phone numbers available\n".format(len(count_numbers(args.total))))
+    elif args.count:
+        print("{} Phone numbers available\n".format(len(count_numbers(args.count))))
     else:
         parser.print_help()
         sys.exit(0)
